1d2da231d64db79873b8e540f35f34ba1f06eaa1
emeliehensfelt <emelie046@gmail.com>
2023-10-03 21:40:58 +0200

    Initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Windows;
+
+namespace WPF_intro
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            cmbPriority.Items.Add("Critical");
+            cmbPriority.Items.Add("High");
+            cmbPriority.Items.Add("Medium");
+            cmbPriority.Items.Add("Low");
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtTask != null && cmbPriority.Text != null)
+            {
+                string task = txtTask.Text;
+                string priority = cmbPriority.Text;
+                Task t = new(task, priority);
+                MyListView.Items.Add(t.DisplayData());
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (txtTask != null && cmbPriority.Text != null)
+            {
+                string task = txtTask.Text;
+                string priority = cmbPriority.Text;
+                Task t = new(task, priority);
+                MyListView.Items.Remove(t.DisplayData());
+            }
+        }
+    }
+}
---
+++ Task.cs
@@ -0,0 +1,22 @@
+﻿namespace WPF_intro
+{
+    internal class Task
+    {
+        private string Name { get; set; }
+        private string Priority { get; set; }
+
+        public Task(string name, string priority)
+        {
+            Name = name;
+            Priority = priority;
+        }
+
+        public string DisplayData()
+        {
+            return $"Task name: {Name}, Priority: {Priority}";
+        }
+
+
+
+    }
+}
